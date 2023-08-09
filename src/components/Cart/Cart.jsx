4baf3be265d04adfa7ca5a15f3da6a931a7c9b66
Alperen Yilan <alp_eren1616@hotmail.com>
2023-08-09 03:09:16 +0300

    Cart Slide güncelleme

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,25 @@
+import "./Cart.css";
+
+const Cart = () => {
+  return (
+    <div className='offcanvas'>
+      <div className='content'>
+        <div className='cart-head'>
+          <h2>sepetim</h2>
+          <a href='/'>X</a>
+        </div>
+        cartItems
+        <div className='total'>
+          <span>Toplam Değer</span>
+          <span>10TL</span>
+        </div>
+        <div className='actions'>
+          <button className='cart-order'>Sipariş Ver</button>
+          <button className='cart-clear'>Temizle</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;

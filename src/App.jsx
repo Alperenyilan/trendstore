bdd04e943c972316326217a252b11c9511480025
Alperen Yilan <alp_eren1616@hotmail.com>
2023-08-12 21:26:53 +0300

    State Duzenlemeleri

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Hero from "./components/Hero/Hero";
 import Header from "./components/Layout/Header";
 import Products from "./components/Products/Products";
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartItem = () => {
+    setCartIsShow(true);
+  };
+
   return (
     <div className='App'>
-      <Cart />
-      <Header />
+      {cartIsShow && <Cart />}
+      <Header onShowCart={showCartItem} />
       <Hero />
       <Products />
     </div>
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,16 +1,17 @@
+/* eslint-disable react/prop-types */
 import "./CartItem.css";
 const CartItem = (props) => {
   return (
     <li className='cart-item'>
       <div className='cart-item-img'>
-        <img src={props.product.img} alt='' />
+        <img src={props.product.img} alt={props.product.name} />
       </div>
       <div className='cart-item-info'>
         <div className='cart-item-texts'>
-          <b>cart item name</b>
+          <b>{props.product.name}</b>
           <div>
-            <span>150 tl x</span>
-            <span>1</span>
+            <span>{props.product.price} x</span>
+            <span>{props.product.amount}</span>
           </div>
         </div>
         <a href='/' className='cart-item-remove'>

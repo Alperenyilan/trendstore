73c81cc7ddeef603a4f3fe9066b5e53f95c3f4e0
Alperen Yilan <alp_eren1616@hotmail.com>
2023-08-15 21:35:51 +0300

    react-portal

---
+++ src/components/UI/Offcanvas.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { Fragment } from "react";
+import "./Offcanvas.css";
+const Offcanvas = (props) => {
+  return (
+    <Fragment>
+      <div className='backdrop' onClick={props.onClose} />
+      <div className='offcanvas'>
+        <div className='content'>{props.children}</div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Offcanvas;

171ddb5f994454828e6fdce2e73e0a161d151a99
Alperen Yilan <alp_eren1616@hotmail.com>
2023-08-19 04:35:24 +0300

    cartReducer

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,10 +1,13 @@
 import Rating from "./Rating";
 import "./ProductItem.css";
 import Card from "../UI/Card";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartProvider";
 // eslint-disable-next-line react/prop-types
 const ProductItem = ({ product }) => {
   // eslint-disable-next-line react/prop-types
   const { name, description, img, price } = product;
+  const { addItem } = useContext(CartContext);
   return (
     <Card>
       <img src={img} alt={name} />
@@ -14,7 +17,9 @@ const ProductItem = ({ product }) => {
         <Rating />
         <span className='price'>{price}</span>
       </div>
-      <button className='add-to-cart'>Sepete Ekle</button>
+      <button className='add-to-cart' onClick={() => addItem(product)}>
+        Sepete Ekle
+      </button>
     </Card>
   );
 };

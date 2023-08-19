dba093c2d366581c5fb3ba8ecc4e092d8b697738
Alperen Yilan <alp_eren1616@hotmail.com>
2023-08-19 04:34:56 +0300

    CartReducer islemleri

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,12 +1,30 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return state;
+  }
+};
+
 const CartProvider = ({ children }) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
   const cartContext = {
     items: [],
     totalAmount: 0,
-    addItem: () => {},
+    addItem: (product) => {
+      dispatchCartAction({ type: "ADD" });
+    },
     removeItem: () => {},
     clearItem: () => {},
   };
